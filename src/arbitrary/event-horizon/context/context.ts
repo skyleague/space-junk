c5fc543253624162ba2fbdbbb671929b3149a927
Paul Visscher <p.e.visscher@skyleague.io>
2022-09-13 21:15:01 +0200

    feat: add requestId to lambda context

--- src/arbitrary/event-horizon/context/context.ts
+++ src/arbitrary/event-horizon/context/context.ts
@@ -2,7 +2,7 @@ import { Context } from '../../../aws/lambda'
 import { mock } from '../../../test/mock'
 
 import type { Arbitrary } from '@skyleague/axioms'
-import { object, constant } from '@skyleague/axioms'
+import { string, object, constant } from '@skyleague/axioms'
 import type { Logger, Tracer, Metrics, LambdaContext } from '@skyleague/event-horizon'
 import { toArbitrary } from '@skyleague/therefore'
 import type { Context as AwsContext } from 'aws-lambda'
@@ -14,6 +14,8 @@ export async function arbitraryContext<C = never, S = never>({ config, services 
         logger: constant(mock<Logger>()),
         tracer: constant(mock<Tracer>()),
         metrics: constant(mock<Metrics>()),
+        requestId: string({ minLength: 2 }),
+        traceId: string({ minLength: 2 }),
         isSensitive: constant(false),
         raw: (await toArbitrary(Context)) as Arbitrary<AwsContext>,
         config: constant(config) as Arbitrary<C>,
