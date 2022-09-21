248a71dbc9837f5e1099436a5939cc28567d3cba
Paul Visscher <p.e.visscher@skyleague.io>
2022-09-21 15:31:21 +0200

    fix(context): add function config, and service types

--- src/arbitrary/event-horizon/context/context.ts
+++ src/arbitrary/event-horizon/context/context.ts
@@ -2,14 +2,15 @@ import { Context } from '../../../aws/lambda'
 import { mock } from '../../../test/mock'
 
 import type { Arbitrary } from '@skyleague/axioms'
-import { string, object, constant } from '@skyleague/axioms'
+import { isFunction, string, object, constant } from '@skyleague/axioms'
 import type { Logger, Tracer, Metrics, LambdaContext } from '@skyleague/event-horizon'
 import { toArbitrary } from '@skyleague/therefore'
 import type { Context as AwsContext } from 'aws-lambda'
 
-export async function arbitraryContext<C = never, S = never>({ config, services }: { config?: C; services?: S } = {}): Promise<
-    Arbitrary<LambdaContext<C, S>>
-> {
+export async function arbitraryContext<C = never, S = never>({
+    config,
+    services,
+}: { config?: C | (() => C); services?: S | (() => S) } = {}): Promise<Arbitrary<LambdaContext<C, S>>> {
     return object({
         logger: constant(mock<Logger>()),
         tracer: constant(mock<Tracer>()),
@@ -18,7 +19,7 @@ export async function arbitraryContext<C = never, S = never>({ config, services 
         traceId: string({ minLength: 2 }),
         isSensitive: constant(false),
         raw: (await toArbitrary(Context)) as Arbitrary<AwsContext>,
-        config: constant(config) as Arbitrary<C>,
-        services: constant(services) as Arbitrary<S>,
+        config: constant(isFunction(config) ? config() : config) as Arbitrary<C>,
+        services: constant(isFunction(services) ? services() : services) as Arbitrary<S>,
     })
 }
